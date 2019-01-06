8c31db87c8dbdfe15e186263ba4cd25f98d24bda
Raed Salah <dkraed@gmail.com>
2019-01-06 15:16:56 +0800

    added links to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,45 @@
 import React from "react";
 
 const NavBar = props => (
-  <nav className="navbar navbar-light bg-light">
-    <span className="navbar-brand mb-0 h1">{props.Title}</span>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#">
+      {props.Title}
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarText"
+      aria-controls="navbarText"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="https://github.com/raedsalah">
+            <span className="badge badge-info">
+              Raed Salah <i className="fab fa-github" />
+            </span>
+          </a>
+        </li>
+        <li className="nav-item active">
+          <a
+            className="nav-link"
+            href="https://github.com/raedsalah/Team-List-react"
+          >
+            <span className="badge badge-info">
+              Git Repo <i className="fas fa-code-branch" />
+            </span>
+          </a>
+        </li>
+      </ul>
+      <span className="navbar-text">
+        A simple React Component for rendering a team list
+      </span>
+    </div>
   </nav>
 );
 
