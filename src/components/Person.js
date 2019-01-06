06e2b580bd7694de9a9c34a04683b3f64f33c8c1
Raed Salah <dkraed@gmail.com>
2019-01-06 15:33:13 +0800

    made the cards fancier

--- src/components/Person.js
+++ src/components/Person.js
@@ -7,8 +7,9 @@ const Person = props => {
         <img src={props.imgSrc} alt="" />
         <div>
           <h4>{props.Title + " " + props.FName}</h4>
-          <h4>{"JOB Title"}</h4>
+          <h5>{"JOB Title"}</h5>
           <p>{props.Email}</p>
+          <hr />
           <p>{props.children}</p>
         </div>
       </div>
